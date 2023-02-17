099e78ac7f57295868eb4a94a84b8d0843c6ef8a
王瑞玉 <wangruiyu@bjknrt.com>
2023-02-17 16:00:31 +0800

    feature(message): 添加下拉弹出菜单

---
+++ lib/ui/tooltip_shape.dart
@@ -0,0 +1,56 @@
+
+import 'package:flutter/material.dart';
+
+/// I'm using [RoundedRectangleBorder] as my reference...
+class TooltipShape extends ShapeBorder {
+  const TooltipShape();
+
+  final BorderSide _side = BorderSide.none;
+  final BorderRadiusGeometry _borderRadius = BorderRadius.zero;
+
+  @override
+  EdgeInsetsGeometry get dimensions => EdgeInsets.all(_side.width);
+
+  @override
+  Path getInnerPath(
+      Rect rect, {
+        TextDirection? textDirection,
+      }) {
+    final Path path = Path();
+
+    path.addRRect(
+      _borderRadius.resolve(textDirection).toRRect(rect).deflate(_side.width),
+    );
+
+    return path;
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    final Path path = Path();
+    final RRect rrect = _borderRadius.resolve(textDirection).toRRect(rect);
+
+    path.moveTo(0, 10);
+    path.quadraticBezierTo(0, 0, 10, 0);
+    path.lineTo(rrect.width - 30, 0);
+    path.lineTo(rrect.width - 20, -10);
+    path.lineTo(rrect.width - 10, 0);
+    path.quadraticBezierTo(rrect.width, 0, rrect.width, 10);
+    path.lineTo(rrect.width, rrect.height - 10);
+    path.quadraticBezierTo(
+        rrect.width, rrect.height, rrect.width - 10, rrect.height);
+    path.lineTo(10, rrect.height);
+    path.quadraticBezierTo(0, rrect.height, 0, rrect.height - 10);
+
+    return path;
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {}
+
+  @override
+  ShapeBorder scale(double t) => RoundedRectangleBorder(
+    side: _side.scale(t),
+    borderRadius: _borderRadius * t,
+  );
+}
