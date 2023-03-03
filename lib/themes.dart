0d4b7042e5ee3ecd556f0058d4477a19e3b18c21
王瑞玉 <wangruiyu@bjknrt.com>
2023-03-03 09:44:44 +0800

    feature(themes): AppBar主题修改

--- lib/themes.dart
+++ lib/themes.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 // 深色模式
-final ThemeData appDarkThemeData = ThemeData.dark().copyWith();
+final ThemeData appDarkThemeData = ThemeData.dark().copyWith(
+    appBarTheme: ThemeData.dark().appBarTheme.copyWith(
+        color: ThemeData.dark().scaffoldBackgroundColor, elevation: 0));
 
 // 浅色模式
-final ThemeData appLightThemeData = ThemeData.light().copyWith();
+final ThemeData appLightThemeData = ThemeData.light().copyWith(
+    appBarTheme: ThemeData.light().appBarTheme.copyWith(
+        color: ThemeData.light().scaffoldBackgroundColor, elevation: 0));
