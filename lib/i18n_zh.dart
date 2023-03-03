b67ec376b884d8c5015f1c2093464b4624356547
王瑞玉 <wangruiyu@bjknrt.com>
2023-03-03 15:25:33 +0800

    feature(login): 修改构建手机登录页

--- lib/i18n_zh.dart
+++ lib/i18n_zh.dart
@@ -15,8 +15,13 @@ class I18nZh {
     "login_account_password_verify_message": "请输入密码",
 
     "login_phone_title": "手机号登录",
+    "login_phone_region": "国家/地区",
+    "login_phone_phone": "手机号",
+    "login_phone_phone_hint_text": "请填写手机号码",
+    "login_phone_use_other_login": "使用其他方式登录",
     "login_phone_bottom_title": "上述手机号仅用于验证",
     "login_phone_bottom_button_title": "同意并继续",
-    "login_phone_phone_verify_message": "请输入正确的手机号"
+    "login_phone_phone_verify_message": "请输入正确的手机号",
+
   };
 }
