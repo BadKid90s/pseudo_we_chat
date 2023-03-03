e40e09cd2cff33bd397b9c32fcfef94a7121f31e
王瑞玉 <wangruiyu@bjknrt.com>
2023-03-03 15:09:14 +0800

    feature(config): 配置国际化
    分开中文和英文文件

--- lib/i18n.dart
+++ lib/i18n.dart
@@ -1,15 +1,9 @@
 import 'package:get/get.dart';
+import 'package:pseudo_we_chat/i18n_en.dart';
+import 'package:pseudo_we_chat/i18n_zh.dart';
 
 class I18nMessages extends Translations {
   @override
-  Map<String, Map<String, String>> get keys => {
-        'zh_CN': {
-          'login_login_button_title': '登录',
-          'login_signup_button_title': '注册',
-        },
-        'en_Us': {
-          'login_login_button_title': 'log In',
-          'login_signup_button_title': 'sign Up',
-        }
-      };
+  Map<String, Map<String, String>> get keys =>
+      {'zh_CN': I18nZh.message, 'en_Us': I18nEn.message};
 }
