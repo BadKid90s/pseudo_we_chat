3a7b70e8501bc59d07fc748e32a2692116a0d4b1
王瑞玉 <wangruiyu@bjknrt.com>
2023-03-12 17:10:02 +0800

    feature(api): 配置HTTP客户端

--- lib/api/api_actual/http.dart
+++ lib/api/api_actual/http.dart
@@ -1,49 +1,73 @@
 import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:pseudo_we_chat/api/exception.dart';
+
+import '../resp_model.dart';
 
 class Http {
   ///超时时间
-  static const int CONNECT_TIMEOUT = 30000;
-  static const int RECEIVE_TIMEOUT = 30000;
+  static const int connectTimeout = 30000;
+  static const int receiveTimeout = 30000;
+  static const String baseUrl = "30000";
+  static List<Interceptor> interceptors = [PrettyDioLogger()];
 
   static late Http instance;
-  static late Dio dio;
+  static late Dio _dio;
 
   factory Http() {
     BaseOptions options = BaseOptions(
-      connectTimeout: const Duration(microseconds: CONNECT_TIMEOUT),
+      connectTimeout: const Duration(microseconds: connectTimeout),
       // 响应流上前后两次接受到数据的间隔，单位为毫秒。
-      receiveTimeout: const Duration(microseconds: RECEIVE_TIMEOUT),
+      receiveTimeout: const Duration(microseconds: receiveTimeout),
       // Http请求头.
       headers: {},
+      baseUrl: baseUrl,
     );
-    dio = Dio(options);
+    _dio = Dio(options);
+    _dio.interceptors.addAll(interceptors);
+
     return instance;
   }
 
-  ///初始化公共属性
-  ///
-  /// [baseUrl] 地址前缀
-  /// [connectTimeout] 连接超时赶时间
-  /// [receiveTimeout] 接收超时赶时间
-  /// [interceptors] 基础拦截器
-  void init(
-      {required String baseUrl,
-      required int connectTimeout,
-      required int receiveTimeout,
-      required List<Interceptor> interceptors}) {
-    dio.options = dio.options.copyWith(
-      baseUrl: baseUrl,
-      connectTimeout: Duration(microseconds: connectTimeout),
-      receiveTimeout: Duration(microseconds: receiveTimeout),
-    );
-    if (interceptors.isNotEmpty) {
-      dio.interceptors.addAll(interceptors);
+  Future<dynamic> _request(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+    Options? options,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      var result = await _dio.request(
+        path,
+        data: data,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        options: options,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return result.data;
+    } on DioError catch (e) {
+      try {
+        var resp = WeChatRespModel.fromJson(e.response?.data);
+        switch (resp.code) {
+          case 500:
+            throw WeChatServerException(resp.message);
+          case 600: // -2
+            throw WeChatCommonException(resp.message);
+          case 403: // -2
+            throw WeChatValidateException(resp.message);
+          default:
+            throw WeChatException(-1, "未知的异常");
+        }
+      } catch (e) {
+        // 其他DioError, 一般都是网络问题
+        throw WeChatException(-1, e.toString());
+      }
+    } catch (e) {
+      throw WeChatException(-1, e.toString());
     }
-  }
-
-  static dynamic get(String path, {Map<String, dynamic>? queryParameters}) {
-    return Http.dio
-        .get(path, queryParameters: queryParameters)
-        .then((value) => value.extra);
   }
 }
---
+++ lib/api/resp_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'resp_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WeChatRespModel _$WeChatRespModelFromJson(Map<String, dynamic> json) =>
+    WeChatRespModel(
+      code: json['code'] as int,
+      message: json['message'] as String?,
+      data: json['data'] as Map<String, dynamic>?,
+    );
+
+Map<String, dynamic> _$WeChatRespModelToJson(WeChatRespModel instance) =>
+    <String, dynamic>{
+      'code': instance.code,
+      'message': instance.message,
+      'data': instance.data,
+    };
